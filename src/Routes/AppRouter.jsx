75d601c94b8c6a85ad8d5963c50dfc5be0966280
chiro45 <luciano.chiroli45@gmail.com>
2022-10-20 22:13:30 -0300

    packagejson

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "../Components/App/User/Home/Home"
 import { Login } from "../Components/Auth/Login/Login"
 import { Register } from "../Components/Auth/Register/Register"
 
@@ -11,6 +12,7 @@ export const AppRouter = () => {
     <Routes>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/homeuser" element={<Home/>}/>
         <Route path="*" element={<Register/>}/>
     </Routes>
   
