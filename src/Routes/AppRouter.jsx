dfa46d01aae2834b5bfa0afe3e9c4ef77e95ee6c
chiro45 <luciano.chiroli45@gmail.com>
2022-10-23 20:33:30 -0300

    appRouter

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "../Components/App/User/Home/Home"
+import { Product } from "../Components/App/User/Product/Product"
 import { Login } from "../Components/Auth/Login/Login"
 import { Register } from "../Components/Auth/Register/Register"
 
@@ -13,6 +14,7 @@ export const AppRouter = () => {
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/homeuser" element={<Home/>}/>
+        <Route path="/product" element={<Product/>}/>
         <Route path="*" element={<Register/>}/>
     </Routes>
   
