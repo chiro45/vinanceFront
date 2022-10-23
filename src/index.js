b1c0cbf7be16f277e5774bb3f1b06b603ed11818
chiro45 <luciano.chiroli45@gmail.com>
2022-10-23 20:32:20 -0300

    import mdb

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 
 import { VinanceApp } from './VinanceApp';
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "./Styles/Global.scss"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
