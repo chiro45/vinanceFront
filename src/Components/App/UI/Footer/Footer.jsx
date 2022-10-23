0eceee97799519772b616b7dad405a8c6faa09ab
chiro45 <luciano.chiroli45@gmail.com>
2022-10-23 20:32:44 -0300

    Footer

--- src/Components/App/UI/Footer/Footer.jsx
+++ src/Components/App/UI/Footer/Footer.jsx
@@ -1,6 +1,88 @@
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='containerFooter w-100'>
+      <MDBFooter className='text-center w-100' color='white' bgColor='dark'>
+      <MDBContainer className='p-4'>
+        <section className='mb-4'>
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='linkedin-in' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='github' />
+          </MDBBtn>
+        </section>
+
+        <section className=''>
+          <form action=''>
+            <MDBRow className='d-flex justify-content-center'>
+              <MDBCol size="auto">
+                <p className='pt-2'>
+                  <strong>Suscribete para recibir novedades</strong>
+                </p>
+              </MDBCol>
+
+              <MDBCol md='5' start='12'>
+                <MDBInput contrast type='email' label='Direccion de email' className='mb-4' />
+              </MDBCol>
+
+              <MDBCol size="auto">
+                <MDBBtn outline color='light' type='submit' className='mb-4'>
+                  Suscribete
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </form>
+        </section>
+
+        <section className='mb-4'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat quaerat
+            voluptatibus placeat nam, commodi optio pariatur est quia magnam eum harum corrupti dicta, aliquam
+            sequi voluptate quas.
+          </p>
+        </section>
+
+        <section className=''>
+          <MDBRow>
+            
+          </MDBRow>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2022 Copyright:
+        <a className='text-white' href=''>
+          Vinance.com
+        </a>
+      </div>
+    </MDBFooter>
+    </div>
   )
 }
