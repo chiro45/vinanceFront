c6f1696dca439b69e7b001e040a56a7eaca23298
chiro45 <luciano.chiroli45@gmail.com>
2022-11-12 16:39:46 -0300

    cardItemList

--- src/Components/App/UI/CardsItemList/CardItemList.jsx
+++ src/Components/App/UI/CardsItemList/CardItemList.jsx
@@ -1,8 +1,9 @@
 import { MDBBtn, MDBIcon } from "mdb-react-ui-kit"
-import { useEffect } from "react"
+import { useState} from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { addActiveProduct, removeActiveProduct } from "../../../../Actions/Product"
+import { ModalModify } from "../Modals/ModalModify/ModalModify"
 import "./CardItemList.scss"
 
 export const CardItemList = ({vino, index, userMode}) => {
@@ -12,25 +13,35 @@ export const CardItemList = ({vino, index, userMode}) => {
     const dispatch = useDispatch()
    
     const handleClickProduct = () => {
-        navigate('/product')
         dispatch(addActiveProduct(vino))
+        navigate('/product')
     }
     const handleClickAddCarrito = () => {
 
     }
-
+    const [openModal,setOpenModal]= useState(false)
+    const handleModify = ()=>{
+        dispatch(removeActiveProduct())
+        dispatch(addActiveProduct(vino))
+        setOpenModal(!openModal)
+    }
     return (
         <div className={"ContainerCardItemList" }>
+            <ModalModify
+            vino={vino}
+            openModal={openModal}
+            handleOpenModal={handleModify}
+            />
             {
                 userMode ==="seller"
                 ?<div className="CardContainerItemList__buttonEdit">
-                    <MDBBtn onClick={handleClickAddCarrito}><MDBIcon fas icon="pen" /></MDBBtn>
+                    <MDBBtn onClick={handleModify}><MDBIcon fas icon="pen" /></MDBBtn>
                 </div>
                 : null
             }
             <div onClick={handleClickProduct}>
                 <div className="ContainerCardItemList__img">
-                    <img src={imagesWine[0].image} /></div>
+                    <img src={imagesWine[0] !== undefined ? imagesWine[0].image  : "https://img.freepik.com/foto-gratis/botella-vino-tinto-uvas_23-2148232185.jpg?w=2000"} /></div>
                 <div className="CardContainerItemList__Varietal"><p>{varietal.varietal}</p></div>
                 <div className="CardContainerItemList__Nombre"><p>{name}</p></div>
                 {
